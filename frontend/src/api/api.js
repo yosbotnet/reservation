6c4f66ed168b836813a55020c3ee072449669bb9
Yosberto Baro Carbonell <yosberto.baro@studio.unibo.it>
2025-02-02 17:08:35 +0100

    fix admin

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -37,11 +37,12 @@ export const api = {
   admin: {
     // User Management
     getUsers: () => api.protected.request('/api/admin/users'),
-    createUser: (userData) => api.protected.request('/api/admin/users', {
+    getSpecializations: () => api.protected.request('/api/admin/specializations'),
+    createDoctor: (doctorData) => api.protected.request('/api/admin/doctors', {
       method: 'POST',
       body: JSON.stringify({
-        ...userData,
-        datanascita: new Date(userData.datanascita).toISOString()
+        ...doctorData,
+        datanascita: new Date(doctorData.datanascita).toISOString()
       })
     }),
     updateUser: (cf, userData) => api.protected.request(`/api/admin/users/${cf}`, {
